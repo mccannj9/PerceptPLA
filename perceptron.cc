4c0c6d1c733d96885454d64b6d64047745e8fa15
Jamie <jamie.mccann@gmx.at>
2019-05-25 17:28:50 +0200

    Committing a function for parsing 'delim' delimited lines

--- perceptron.cc
+++ perceptron.cc
@@ -1,4 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> split(const string& str, char delim) {
+  // A function for parsing delimited lines of data
+  vector<string> tokens;
+  size_t prev = 0, pos = 0;
+  do {
+    pos = str.find(delim, prev);
+    if (pos == string::npos) {
+      pos = str.length();
+    }
+    string token = str.substr(prev, pos-prev);
+    if (!token.empty()) {
+      tokens.push_back(token);
+    }
+    prev = pos + 1;
+
+  }
+  while (pos < str.length() && prev < str.length());
+
+  return tokens;
+}
+
 
 int main(int argc, char** argv) {
   return 0;
