aed0f8065c7a1d9a84b165dac334e409ad0c4c06
Jamie <jamie.mccann@gmx.at>
2019-05-26 09:57:00 +0200

    Made template function for loading data points and labels with different types

--- perceptron.cc
+++ perceptron.cc
@@ -26,11 +26,12 @@ vector<string> split(const string& str, char delim) {
   return tokens;
 }
 
-vector<double> load_data(char* filename) {
+template<typename T>
+vector<T> load_data(char* filename, vector<T>& data) {
   ifstream datafile;
   datafile.open(filename);
 
-  vector<double> data;
+  // vector<T> data;
 
   if (datafile.is_open()) {
     string line;
@@ -51,11 +52,18 @@ vector<double> load_data(char* filename) {
   return data;
 }
 
-
 int main(int argc, char** argv) {
 
-  vector<double> data = load_data(argv[1]);
-  cout << "Size=" << data.size() << endl;
+  vector<double> data;
+  data = load_data(argv[1], data);
+  vector<short> labels;
+  labels = load_data(argv[2], labels);
+  cout << "Data Size=" << data.size() << endl;
+  cout << "Labels Size=" << labels.size() << endl;
+
+  // for (size_t i = 0; i < data.size(); i++) {
+  //   cout << data.at(i) << endl;
+  // }
 
   return 0;
 }
